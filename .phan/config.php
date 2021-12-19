ee405e3ab8c25e754d492128c8c6cb4ce1d9a987
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2021-12-19 03:23:46 +0000

    build: Updating mediawiki/mediawiki-phan-config to 0.11.1
    Change-Id: I54aaea96253374b9f10d5063318c659683945867

--- .phan/config.php
+++ .phan/config.php
@@ -1,2 +1,8 @@
 <?php
-return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$config = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+// The target of php8 would require to replace resource in MultiHttpClient.php
+// But the classes CurlHandle and CurlMultiHandle not exists in php7 to be replaced with.
+$config['target_php_version'] = '7.4';
+
+return $config;

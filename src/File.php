3330ce1efd4a8143ee2525ad9f48654dd84c826c
Umherirrender <umherirrender_de.wp@web.de>
2024-11-15 14:23:45 +0100

    Use explicit nullable type on parameter arguments (for PHP 8.4)
    Implicitly marking parameter $... as nullable is deprecated in PHP
    8.4. The explicit nullable type must be used instead.
    
    Change-Id: I47eb23307804d7d9ba8f629b702f644414ef4183

--- src/File.php
+++ src/File.php
@@ -429,7 +429,7 @@ class File extends \File {
 	}
 
 	/** @inheritDoc */
-	public function getUploader( int $audience = self::FOR_PUBLIC, Authority $performer = null ): ?UserIdentity {
+	public function getUploader( int $audience = self::FOR_PUBLIC, ?Authority $performer = null ): ?UserIdentity {
 		if ( isset( $this->mInfo['user'] ) ) {
 			// We don't know if the foreign repo will have a real interwiki prefix,
 			// treat this user as a foreign imported user. Maybe we can do better?
@@ -443,7 +443,7 @@ class File extends \File {
 	 * @param Authority|null $performer
 	 * @return null|string
 	 */
-	public function getDescription( $audience = self::FOR_PUBLIC, Authority $performer = null ) {
+	public function getDescription( $audience = self::FOR_PUBLIC, ?Authority $performer = null ) {
 		return isset( $this->mInfo['comment'] ) ? strval( $this->mInfo['comment'] ) : null;
 	}
 
@@ -571,7 +571,7 @@ class File extends \File {
 	 * @return string|false HTML
 	 * @return-taint escaped
 	 */
-	public function getDescriptionText( \Language $lang = null ) {
+	public function getDescriptionText( ?\Language $lang = null ) {
 		global $wgLang;
 
 		if ( !$this->repo || !$this->repo->fetchDescription ) {
